010b10e8758da11eda0bf8700f7b9ae4d2abd5bc
murabye <vlada_kuz2011@mail.ru>
2017-06-10 15:56:29 +0500

    создание своего класса ошибок для выхода за пределы допустимого диапазона даты

---
+++ CommandWork/DataOutOfRange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+// свой класс ошибок для выхода за допустимые границы даты
+namespace CommandWork
+{
+    class DataOutOfRange : ArgumentOutOfRangeException
+    {
+
+        public DataOutOfRange()
+        {
+        }
+
+        public DataOutOfRange(string message)
+            : base(message)
+        {
+        }
+
+        public DataOutOfRange(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
