f61221311a17897878c67acdc5df97352801e1e0
murabye <vlada_kuz2011@mail.ru>
2017-06-11 12:44:04 +0500

    добавление класса протокол

---
+++ CommandWork/Protocol.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandWork
+{
+    class Protocol
+    {
+        // поля
+        private string _arg1, _arg2;
+        private bool _isPlus;
+        private Dictionary<string, string> _operations;
+
+        // публичные свойства
+        public string Input
+        {
+            get
+            {
+                var ans = _isPlus ? _arg1 + '+' + _arg2 : _arg1 + '-' + _arg2;
+                return ans;
+            }
+        }
+        public Dictionary<string, string> Operations { get { return _operations; } }
+
+        // добавление операции
+        public void AddOperation(string op, string result)
+        {
+            _operations.Add(op, result);
+        }
+    }
+}
