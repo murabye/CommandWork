d7b2656f0257571ce3d087f8ef00dee2122a8999
murabye <vlada_kuz2011@mail.ru>
2017-06-11 13:00:25 +0500

    проработка протокола

--- CommandWork/Protocol.cs
+++ CommandWork/Protocol.cs
@@ -10,7 +10,7 @@ namespace CommandWork
     public class Protocol
     {
         // поля
-        private string _arg1, _arg2;
+        private string _arg1, _arg2, _path;
         private bool _isPlus;
         private Dictionary<string, string> _operations;
 
@@ -25,10 +25,33 @@ namespace CommandWork
         }
         public Dictionary<string, string> Operations { get { return _operations; } }
 
+        // конструктор
+        public Protocol(string path, string arg1, string arg2, bool isPlus)
+        {
+            // чистый протокол
+            _arg1 = arg1;
+            _arg2 = arg2;
+            _path = path;
+            _isPlus = isPlus;
+            _operations = new Dictionary<string, string>();
+        }
+
         // добавление операции
         public void AddOperation(string op, string result)
         {
             _operations.Add(op, result);
+        }
+
+        // exceptions
+        public void AddException(string op, string exception)
+        {
+            _operations.Add(op, exception);
+        }
+
+        // запись в файл
+        public void Finish()
+        {
+            // запись в файл, находящийся по адресу _path
         }
     }
 }
