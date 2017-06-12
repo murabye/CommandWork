6dc1c8b3a1203614a53d67fc4a686329af14455c
TemaSolovei <temasolovei@gmail.com>
2017-06-12 19:01:11 +0500

    Реализовал функцию CheckDate - проверка входной даты на корректные значения

--- CommandWork/Calc.cs
+++ CommandWork/Calc.cs
@@ -16,7 +16,141 @@ namespace CommandWork
         // если будешь убирать у себя это - скажи мне
         public static bool CheckDate(string[] date, bool isAmerican) // тут проверить на 30 февраля
         {
-            return false;
+            int days, months, years;
+            bool leapYear = false; // Високосность года
+
+            if (CheckNumber(date[2], 1583, 9999))
+            {
+                years = Int32.Parse(date[2]);
+                if ((years % 4) == 0) leapYear = true; // Проверка на високосность года
+            }
+            else return false;
+
+            if (CheckNumber(date[1], 1, 12))
+            {
+                months = Int32.Parse(date[1]);
+            }
+            else return false;
+
+            switch (months)
+            {
+                case 1:
+                    if (CheckNumber(date[0], 1, 31))
+                    {
+                        days = Int32.Parse(date[0]);
+                    }
+                    else return false;
+                    break;
+
+                case 2:
+                    if (!leapYear && CheckNumber(date[0], 1, 28))
+                    {
+                        days = Int32.Parse(date[0]);
+                    }
+                    else if (leapYear && CheckNumber(date[0], 1, 29))
+                    {
+                        days = Int32.Parse(date[0]);
+                    }
+                    else return false;
+                    break;
+
+                case 3:
+                    if (CheckNumber(date[0], 1, 31))
+                    {
+                        days = Int32.Parse(date[0]);
+                    }
+                    else return false;
+                    break;
+
+                case 4:
+                    if (CheckNumber(date[0], 1, 30))
+                    {
+                        days = Int32.Parse(date[0]);
+                    }
+                    else return false;
+                    break;
+
+                case 5:
+                    if (CheckNumber(date[0], 1, 31))
+                    {
+                        days = Int32.Parse(date[0]);
+                    }
+                    else return false;
+                    break;
+
+                case 6:
+                    if (CheckNumber(date[0], 1, 30))
+                    {
+                        days = Int32.Parse(date[0]);
+                    }
+                    else return false;
+                    break;
+
+                case 7:
+                    if (CheckNumber(date[0], 1, 31))
+                    {
+                        days = Int32.Parse(date[0]);
+                    }
+                    else return false;
+                    break;
+
+                case 8:
+                    if (CheckNumber(date[0], 1, 31))
+                    {
+                        days = Int32.Parse(date[0]);
+                    }
+                    else return false;
+                    break;
+
+                case 9:
+                    if (CheckNumber(date[0], 1, 30))
+                    {
+                        days = Int32.Parse(date[0]);
+                    }
+                    else return false;
+                    break;
+
+                case 10:
+                    if (CheckNumber(date[0], 1, 31))
+                    {
+                        days = Int32.Parse(date[0]);
+                    }
+                    else return false;
+                    break;
+
+                case 11:
+                    if (CheckNumber(date[0], 1, 30))
+                    {
+                        days = Int32.Parse(date[0]);
+                    }
+                    else return false;
+                    break;
+
+                case 12:
+                    if (CheckNumber(date[0], 1, 31))
+                    {
+                        days = Int32.Parse(date[0]);
+                    }
+                    else return false;
+                    break;
+            }
+
+            if (!CheckNumber(date[3], 0, 23))
+            {
+                return false;
+            }
+
+            if (!CheckNumber(date[4], 0, 59))
+            {
+                return false;
+            }
+
+            if (!CheckNumber(date[5], 0, 59))
+            {
+                return false;
+            }
+
+            return true;
         }
 
         public static bool CheckMonths(string days) // лучше оставь это. я тут проверяю корректно ли введено значение дней или месяцев
